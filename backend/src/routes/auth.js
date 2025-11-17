0b55e4ee49c8179cc9fd4d0492572c72f452ad37
ali44ashhad <ashhadalialvi@gmail.com>
2025-11-17 12:25:13 +0530

    remove authentication from get currentuser

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -30,7 +30,7 @@ router.post('/login', loginValidation, login);
 router.post('/logout', logout);
 
 // Protected routes
-router.get('/me', authenticate, getCurrentUser);
+router.get('/me',  getCurrentUser);
 router.put('/change-password', authenticate, changePasswordValidation, changePassword);
 
 module.exports = router;
